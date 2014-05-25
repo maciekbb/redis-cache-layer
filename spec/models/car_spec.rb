30c7f76a13f32dd272f6800d22f5e579f620cc7c
Maciek Wątroba <maciek.bb@gmail.com>
2014-05-25 11:19:35 +0200

    some specs

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -4,33 +4,72 @@ describe Car, :type => :model do
   before(:all) do
     colors = ["red", "green", "blue"]
     names = ["toyota", "vw", "opel"]
-    1000.times do
+    100.times do
       Car.create!(color: colors.sample, name: names.sample, speed: rand(1000))
     end
+
+    $redis.flushall
   end
 
   after(:all) do
     Car.destroy_all
   end
 
-  it "top" do
-    benchmark("top") do
-      Car.order("speed" => :desc).limit(500).length
+  describe "correctness" do
+    it "fetches all cars" do
+      expect(Car.rall.length).to eq Car.count
     end
+
+    describe "where" do
+      it "one condition" do
+        expect(Car.rwhere(color: "red").length).to eq Car.where(color: "red").count
+      end
+
+      it "two conditions" do
+        expect(Car.rwhere(color: "red", name: "toyota").length).to eq Car.where(color: "red", name: "toyota").count
+      end
+    end
+
+    it "generates top list" do
+      expect(Car.rtop("speed", 20).last["speed"].to_i).to eq Car.order(speed: :desc).limit(21)[-1]["speed"]
+    end
+
   end
 
-  it "rtop" do
-    benchmark("rtop") do
-      Car.rtop("speed", 500).length
+  describe "performance" do
+
+
+    it "top" do
+      benchmark("top") do
+        Car.order("speed" => :desc).limit(50).length
+      end
+    end
+
+    it "rtop" do
+      benchmark("rtop") do
+        Car.rtop("speed", 50).length
+      end
+    end
+
+    it "where" do
+      benchmark("where") do
+        Car.where(color: "red", name: "toyota").length
+      end
+    end
+
+    it "rwhere" do
+      benchmark("rwhere") do
+        Car.rwhere(color: "red", name: "toyota").length
+      end
     end
   end
 end
 
 
 def benchmark(desc, &block)
-  # block.call
+  block.call
   time = Benchmark.realtime do
-    1.times do
+    50.times do
       block.call
     end
   end
